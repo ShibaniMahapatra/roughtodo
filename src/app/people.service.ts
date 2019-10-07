0c44b90187e244d0bdd08a8da2ea1da72e6ced64
Shibani Mahapatra <shibani.mahapatra47@gmail.com>
2019-10-07 14:41:56 +0530

    First commit

--- src/app/people.service.ts
+++ src/app/people.service.ts
@@ -3,7 +3,7 @@ import {HttpClientModule} from '@angular/common/http';
 
 import {HttpClient} from '@angular/common/http';
 import { Observable ,of} from 'rxjs';
-import { AngularFireDatabase, FirebaseListObservable } from '@angularfire2/database';
+// import { AngularFireDatabase, FirebaseListObservable } from '@angularfire2/database';
 
 
 @Injectable({
@@ -12,17 +12,17 @@ import { AngularFireDatabase, FirebaseListObservable } from '@angularfire2/datab
 export class PeopleService {
 
   constructor(private http: HttpClient) { }
-  // fetchPeople():Observable<Object>
-  // {
-  //   // return(this.http.get('http://127.0.0.1:5000/todo'))
-  //   return this.http.get("http://127.0.0.1:5000/todo");
-  //   // .map(res => res.json())x
-  // }
-  
+  fetchPeople():Observable<Object>
+  {
+    // return(this.http.get('http://127.0.0.1:5000/todo'))
+    return this.http.get("http://127.0.0.1:5000/todo")["data"];
+    // .map(res => res.json())x
+  }
+
   // fetchPeople():FirebaseListObservable<any[]>
   // {
   //   // return(this.http.get('http://127.0.0.1:5000/todo'))
   //   return this.http.get("http://127.0.0.1:5000/todo");
   //   // .map(res => res.json())x
-  }
+  // }
 }
